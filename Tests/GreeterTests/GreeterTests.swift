125d4320228877dc997e5a26b49d093a1ea89a1f
Raja Rahul <rajarahul@Rajas-MacBook-Pro.local>
2019-01-17 14:10:33 +0530

    Adding Files

---
+++ Tests/GreeterTests/GreeterTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import Greeter
+
+final class GreeterTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Greeter().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import GreeterTests
+
+var tests = [XCTestCaseEntry]()
+tests += GreeterTests.allTests()
+XCTMain(tests)
